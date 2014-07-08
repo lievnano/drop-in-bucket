54140f1966c4d4f94236fe85b386f5d002e392a4
liev nano <lievnano@gmail.com>
2014-07-08 14:15:15 -0700

    added signup/login and sessions

---
+++ bucket-server.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var partials = require('express-partials');
+var db = require('./app/config');
+var Users = require('./app/collections/users');
+var User = require('./app/models/user');
+var Buckets = require('./app/collections/buckets');
+var Bucket = require('./app/models/bucket');
+var Pledges = require('./app/collections/pledges');
+var Pledge = require('./app/models/pledge');
+var Drops = require('./app/collections/drops');
+var Drop = require('./app/models/drop');
+var session = require('express-session')
+
+var app = express();
+
+app.use(session({
+  secret: 'hdshfaeurp33afad687N3XCND342',
+  cookie: {maxAge : 1800};
+  // cookie: {secure: true}, set later once https setup...
+
+}));
+//change secret when using in production...
+
+
+var expressHbs = require('express3-handlebars')
+app.engine('hbs', expressHbs({extname:'hbs',defaultLayout:'main.hbs'}));
+
+// app.use( express.static(__dirname + '/public'));
+// app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+  //get trending buckets
+  //get latest buckets
+  //show user buckets -- if logged in...
+  var data = {}; , assign various objects
+  res.render('index', data); //render it
+});
+
+app.get('/login', function(req,res){
+  res.render('login');
+});
+
+app.get('/signup', function(req,res){
+  res.render('signup');
+});
+
+app.get('/bucket/:url', function(req,res){
+  //see if bucket url exists and get data if so or go to location if not
+
+  // if exists render with data
+  res.render('bucket', data);
+});
+
+app.get('/user', function(req,res){
+  //get user data
+
+  res.render('user', data);
+});
+
+app.post('/signup', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var email = req.body.email;
+  // check to see if username is valid
+  // if (!util.isValidUsername(username)){
+  //   console.log('some shit');
+  //   return res.send(404);
+  // }
+  // if (!util.isValidPassword(password)){
+  //   console.log('some oher shit');
+  //   return res.send(404);
+  // }
+  new User({username: username}).fetch().then(function(found) {
+    if (found) {
+      console.log('tell user they need a unique userid');
+      res.render('/signup', {error: 'Username already in use!'});
+    } else {
+      console.log('here');
+      var user = new User({
+        username: username,
+      });
+      console.log('caught', user);
+      user.saveToDB(password, function(newUser){
+        console.log('what', user);
+        Users.add(user);
+        res.render('login');
+      });
+    }
+  });
+
+app.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var lastpage = req.session.lastpage;
+  new User({username: username}).fetch().then(function(user) {
+    if (user) {
+      user.checkPassword(password, function(result) {
+        if (result) {
+          req.session.regenerate(function(err){
+            if (err) throw err;
+            req.session.user = user.get('id');
+            if (lastpage){
+              res.redirect(lastpage);
+            }
+            else{
+              res.redirect('/');
+            }
+          })
+        } else {
+          console.log('Bad password');
+          res.render('/login', {error:'Incorrect Username or Password'});
+        }
+      });
+    }
+    else{
+      console.log('non-existant Username');
+      res.render('/login', {error:'Incorrect Username or Password'});
+    }
+  });
+});
+
+app.post('/create', function(req,res){
+  //check if user is logged in and authorized
+  //check if bucket exists...
+  
+});
+
+app.post('/bucket:url', function(req,res){
+
+});
